884945fe72040977f82d5b76ef0971d576198d17
giovannizinzi <jzooms@gmail.com>
2021-09-07 01:37:06 +0000

    going through figment tutorial

--- pages/api/solana/connect.ts
+++ pages/api/solana/connect.ts
@@ -8,11 +8,16 @@ export default async function connect(
 ) {
   try {
     const url = getSafeUrl();
-    const connection = undefined;
-    const version = undefined;
+    const connection = new Connection(url, "confirmed");
+    const version = await connection.getVersion();
     res.status(200).json(version?.["solana-core"]);
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
   }
 }
+
+
+// Documentation for connection can be found https://solana-labs.github.io/solana-web3.js/classes/Connection.html
+// Connection establishes a connection to a fullnode JSON RPC endpoint
+// look at constructor in future. 
